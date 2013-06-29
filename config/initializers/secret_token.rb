a20781bdf06d0968616451a699f62da9976c887f
Stuart Terrett <shterrett@gmail.com>
2013-06-29 06:57:51 -0400

    Initial commit; gem and db setup. Sass, Foundation, Rspec, Postgres, Haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StuartTerrett::Application.config.secret_key_base = 'c887b390c852dcf7d41a9d40f043ab19ae206170ae8d44c70c9a0c9709554fa134b0369370b19a6fb728cc80dd5e9c41f13afaea624c3e76e52b871f14c9e2ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StuartTerrett::Application.config.session_store :cookie_store, key: '_stuart_terrett_session'
