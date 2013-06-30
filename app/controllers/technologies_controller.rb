31ca2b9578dc5e9ece7387b8aab4cc9e4b1150ae
Stuart Terrett <shterrett@gmail.com>
2013-06-30 14:56:45 -0400

    Edit and Update actions for Technologies

--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -5,12 +5,33 @@ class TechnologiesController < ApplicationController
   end
   
   def create
+    @technology = Technology.new(tech_params)
+    if @technology.save
+      flash[:success] = "Technology created successfully"
+      redirect_to new_technology_path
+    else
+      flash[:error] = "There was an error creating the technology"
+      render 'new'
+    end
   end
   
   def edit
+    @technology = Technology.find(params[:id])
   end
   
   def update
+    @technology = Technology.find(params[:id])
+    if @technology.update_attributes(tech_params)
+      flash[:success] = "Technology updated successfully"
+      redirect_to technologies_path
+    else
+      flash[:error] = "There was an error updating the technology"
+      render 'edit'
+    end
+  end
+  
+  def tech_params
+    params.require(:technology).permit(:name, :abbreviation)
   end
   
 end
--- spec/features/technologies_spec.rb
+++ spec/features/technologies_spec.rb
@@ -9,4 +9,40 @@ feature "New Technology" do
     page.should have_button "Create Technology"
   end
   
+  scenario "create new technology" do
+    visit '/technologies/new'
+    fill_in "Name", with: "Ruby"
+    fill_in "Abbreviation", with: "ruby"
+    expect do
+      click_button "Create Technology"
+    end.to change(Technology, :count).by(1)
+    page.should have_text "Technology created successfully"
+  end
+  
 end    
+
+feature "Edit Technology" do
+  before(:all) { FactoryGirl.create(:technology) }
+  
+  scenario "visit /technologies/:id/edit" do
+    tech = Technology.all.first
+    id = tech.id
+    visit "/technologies/#{id}/edit"
+    page.should have_field "Name"
+    page.should have_field "Abbreviation"
+    page.should have_button "Update Technology"
+  end
+  
+  scenario "edit technology" do
+    tech = Technology.all.first
+    visit "/technologies/#{tech.id}/edit"
+    fill_in "Name", with: "EditedName"
+    fill_in "Abbreviation", with: "edits"
+    click_button "Update Technology"
+    new_tech = Technology.find(tech.id)
+    new_tech.name.should == "EditedName"
+    new_tech.abbreviation.should == "edits"
+    page.should have_text "Technology updated successfully"
+  end
+  
+end
