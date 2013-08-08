6be040e8d59ae247b5e5ede9128e636e219dd070
Stuart Terrett <shterrett@gmail.com>
2013-08-07 22:23:57 -0400

    Removes initialize test for abouts; initialization handled with find_or_create_by() in controller on first visit. Tested in feature spec.

--- spec/models/about_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe About do
-
-  it "should initialize a single instance on startup if one does not currently exist" do
-    About.all.length.should == 1
-  end
-
-end
