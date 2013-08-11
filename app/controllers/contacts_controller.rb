631d19747745fe68e7fcf74c582be6cd3f44467f
Stuart Terrett <shterrett@gmail.com>
2013-08-11 09:07:05 -0400

    Sets up email contact for website

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,12 +6,29 @@ class ContactsController < ApplicationController
   
   def create
     @contact = Contact.new(contact_params)
-    # send email 
-    render 'new'
+    if @contact.valid?
+      email = ContactsMailer.contact_form(@contact)
+      send_message(email)
+    else
+      flash.now[:error] = "The contact form is not valid; please complete all fields and check your email address."
+      render 'new'
+    end
   end
   
   def contact_params
     params.require(:contact).permit(:name, :email, :subject, :body)
   end
-  
+
+  def send_message(email)
+    begin
+     email.deliver
+     flash[:success] = "Thank you for your message!"
+     redirect_to root_path
+    rescue => err
+      logger.warn(err)
+      flash.now[:error] = "There was an error delivering your message; please try again. If this error persists, please wait and try later."
+      render 'new'
+    end
+  end
+
 end
--- spec/features/contacts_spec.rb
+++ spec/features/contacts_spec.rb
@@ -8,7 +8,25 @@ feature "Create new contact form" do
     page.should have_field "contact_email"
     page.should have_field "Subject"
     page.should have_field "Body"
+    page.should have_button "Send Message"
   end
-    
+
+  scenario "Invalid form" do
+    visit "/contact-me"
+    click_button "Send Message"
+    page.should have_text "not valid"
+    current_path.should == "/contacts"
+  end  
+
+  scenario "Valid form" do
+    visit "/contact-me"
+    fill_in "Name", with: "Stuart"
+    fill_in "contact_email", with: "stuart@example.com"
+    fill_in "Subject", with: "Test"
+    fill_in "Body", with: "TEST"
+    click_button "Send Message"
+    page.should have_text "Thank you"
+    current_path.should == root_path
+  end
   
 end
