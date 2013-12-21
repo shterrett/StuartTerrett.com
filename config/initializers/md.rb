7a81dae3639c1c2f158153accd84ad3e94b99f4e
Stuart Terrett <shterrett@gmail.com>
2013-12-21 12:41:35 -0500

    Proper newline in markdown <code>

--- config/initializers/md.rb
+++ config/initializers/md.rb
@@ -1,17 +1,14 @@
 module Md
-
   class << self
-    
     attr_accessor :parser
-    
+
     def parser
       @parser || self.init_markdown
-    end 
-    
-    def init_markdown
-      Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     end
 
+    def init_markdown
+      renderer = Redcarpet::Render::HTML.new(prettify: true)
+      Redcarpet::Markdown.new(renderer, { fenced_code_blocks: true })
+    end
   end
-  
 end
